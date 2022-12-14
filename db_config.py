0feb042bfc1bb1b625538ebef0ad9ffdf1a17973
iliyanaaaa <nospliss@gmail.com>
2022-12-14 08:13:13 +0100

    Change MySQL pipeline DB from test to production

--- db_config.py
+++ db_config.py
@@ -7,22 +7,19 @@ This is a temporary script file.
 Author: iliyana.tarpova@sparrks.de
 """
 
-
-
-
+from urllib.parse import quote_plus
 
 import sqlalchemy
 from load_credentials import load_sparrksapp_db_credentials as load
 
+db_prod_or_test = 'prod_'  # 'test_'
 
 ### Credentials
-USER = load('username')
-PASSWORD = load('password')
-SERVER = load('server')
-DATABASE = load('database')
-PORT = load('port')
-SQA_CONN_STR = f"mysql+pymysql://{USER}:{PASSWORD}@{SERVER}:{PORT}/{DATABASE}"
+USER = load(db_prod_or_test + 'username')
+PASSWORD = load(db_prod_or_test + 'password')
+SERVER = load(db_prod_or_test + 'server')
+DATABASE = load(db_prod_or_test + 'database')
+PORT = load(db_prod_or_test + 'port')
+SQA_CONN_STR = f"mysql+pymysql://{USER}:%s@{SERVER}:{PORT}/{DATABASE}" % quote_plus(PASSWORD)
 SQA_CONN_PUB_ENGINE = sqlalchemy.create_engine(SQA_CONN_STR)
 SQA_CONN_PUB = SQA_CONN_PUB_ENGINE.connect()
-
-
--- db_queries.py
+++ db_queries.py
@@ -2,7 +2,7 @@ get_mysql_table_names_query = '''
 SELECT table_name
 FROM information_schema.tables
 WHERE TABLE_TYPE = "BASE TABLE"
-AND TABLE_SCHEMA = 'sparrks'
+AND TABLE_SCHEMA = '{}'
 '''
 
 get_mysql_table_schemas_query = '''
@@ -11,4 +11,4 @@ SHOW CREATE TABLE {}
 
 get_data_from_mysql_query = '''
 select * 
-from sparrks.{} '''
+from {}.{} '''
